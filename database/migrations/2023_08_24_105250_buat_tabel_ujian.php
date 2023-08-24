eba33e7bf31b3e274da2fca7e08c0d91077fb439
nasrul <2006065@itg.ac.id>
2023-08-24 11:08:44 +0700

    menambahkan migration tabel ujian

---
+++ database/migrations/2023_08_24_105250_buat_tabel_ujian.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class BuatTabelUjian extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ujian', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_uian',100);
+            $table->string('nama_mapel',100);
+            $table->string('jumlah_soal',100);
+            $table->string('durasi');
+            $table->string('deskripsi');
+            $table->string('acak_soal',['Y','N']);
+            $table->string('acak_jawaban',['Y','N']);
+            $table->string('tampilan_hasil',['Y','N']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ujian');
+    }
+}
